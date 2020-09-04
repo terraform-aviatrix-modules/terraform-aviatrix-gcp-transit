450101fe09990342a57782daf947b630a27d2a4c
Dennizz <root@ipaddr.nl>
2020-09-04 19:34:35 +0200

    add descriptions

--- variables.tf
+++ variables.tf
@@ -1,27 +1,33 @@
 variable "primary_region" {
   description = "Primary GCP region where subnet and Aviatrix Transit Gateway will be created"
+  type = string
 }
 
 variable "ha_region" {
   description = "Secondary GCP region where subnet and HA Aviatrix Transit Gateway will be created"
   default     = ""
+  type = string
 }
 
 variable "account" {
   description = "Name of the GCP Access Account defined in the Aviatrix Controller"
+  type = string
 }
 
 variable "instance_size" {
   description = "Size of the compute instance for the Aviatrix Gateways"
   default     = "n1-standard-1"
+  type = string
 }
 
 variable "sub1_cidr" {
   description = "CIDR of the primary GCP subnet"
+  type = string  
 }
 
 variable "sub2_cidr" {
   description = "CIDR of the HA GCP subnet"
+  type = string
   default     = ""
 }
 
@@ -32,28 +38,31 @@ variable "ha_gw" {
 }
 
 variable "az1" {
+  description = "Concatenates with primary_region to form az names. e.g. us-east1b. Used for insane mode only."
   type    = string
   default = "b"
 }
 
 variable "az2" {
+  description = "Concatenates with ha_region to form az names. e.g. us-east4b. Used for insane mode only."  
   type    = string
-  default = "c"
+  default = "b"
 }
 
 variable "name" {
+  description = "Name for this spoke VPC and it's gateways"
   type    = string
   default = ""
 }
 
 variable "connected_transit" {
-  description = ""
+  description = "Set to false to disable connected transit."
   type        = bool
   default     = true
 }
 
 variable "active_mesh" {
-  description = ""
+  description = "Set to false to disable active mesh."
   type        = bool
   default     = true
 }
